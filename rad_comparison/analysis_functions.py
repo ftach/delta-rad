1c9adbdfcd0ecb70eed35766eeb76e4fdff666e5
Florent Tachenne <tachenne.florent@gmail.com>
2025-01-20 15:10:24 +0100

    tried to recompute auc but failed, beginning another training with random split

--- rad_comparison/analysis_functions.py
+++ rad_comparison/analysis_functions.py
@@ -4,6 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neural_network import MLPClassifier
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.naive_bayes import GaussianNB
+from predictions import train_model
+from dataset import get_dataset, min_max_scaling
+from typing import Sequence
+
 
 def plot_heatmap(results: dict, table: str, outcome: str, feat_sel_algo_list: list, pred_algo_list: list, metrics: list = ['roc_auc', 'sensitivity', 'specificity'], value: str = 'max'): 
     """
@@ -99,17 +120,23 @@ def get_best_results(results: dict, delta_rad_tables: list, feat_sel_algo_list: 
     """
 
     top_results = {}
-    print(f"Top 3 results for each table and {outcome} in terms of {metric}:")
+    print(f"Top {k} results for each table and {outcome} in terms of {metric}:")
     for table in delta_rad_tables: 
         top_results[table] = {}
         results_list = []
         for feat_sel_algo in feat_sel_algo_list:
             for pred_algo in pred_algo_list:
                 values = [results[table][feat_sel_algo][pred_algo][outcome][nb_features][metric] for nb_features in results[table][feat_sel_algo][pred_algo][outcome].keys()]
+                features = [results[table][feat_sel_algo][pred_algo][outcome][nb_features]['features'] for nb_features in results[table][feat_sel_algo][pred_algo][outcome].keys()]
+                params = [results[table][feat_sel_algo][pred_algo][outcome][nb_features]['params'] for nb_features in results[table][feat_sel_algo][pred_algo][outcome].keys()]
                 filtered_values = [x for x in values if (x != 'N/A') and (x != 0) and (x != 'None')]
                 if len(filtered_values) > 0:
                     max_value = max(filtered_values)
-                    results_list.append((max_value, pred_algo, feat_sel_algo, len(filtered_values)))
+                    # get index in the list of the max value
+                    index = filtered_values.index(max_value)
+                    params = params[index]
+                    features = features[index] # get features for which the metric is the max
+                    results_list.append((max_value, pred_algo, feat_sel_algo, features, params))
 
         # Sort the results list by the max value in descending order and take the top k
         if len(results_list) > 0:
@@ -119,9 +146,178 @@ def get_best_results(results: dict, delta_rad_tables: list, feat_sel_algo_list: 
         # Display the top k results for each table and each outcome
         print(f"Top {k} results for table {table}:")
         for result in results_list:
-            print(f"{metric}: {result[0]}, Prediction Algorithm: {result[1]}, Feature Selection Algorithm: {result[2]}, Number of Features: {result[3]}")
+            print(f"{metric}: {result[0]}, Prediction Algorithm: {result[1]}, Feature Selection Algorithm: {result[2]}, Features: {result[3]}")
         print("\n")
     return top_results
+
+def retrain_best_models(result, X_train, y_train):
+    # Get info from best models and retrain them
+    # select features in X_train and X_test
+    
+    params = result[4]
+    pred_algo = result[1]
+
+    if pred_algo == 'RF': 
+        clf = RandomForestClassifier(**params)
+    elif pred_algo == 'ADABOOST':
+        clf = AdaBoostClassifier(**params) 
+    elif pred_algo == 'LOGREG':
+        clf = LogisticRegression( **params) 
+    elif pred_algo == 'LOGREGRIDGE':
+        clf = LogisticRegression(**params) 
+    elif pred_algo == 'DT':
+        clf = DecisionTreeClassifier(**params) 
+    elif pred_algo == 'PSVM':
+        clf = SVC(**params)
+    elif pred_algo == 'KNN':
+        clf = KNeighborsClassifier(**params)
+    elif pred_algo == 'BAGG':
+        clf = BaggingClassifier( **params)
+    elif pred_algo == 'MLP':
+        clf = MLPClassifier(**params)
+    elif pred_algo == 'LDA':
+        clf = LinearDiscriminantAnalysis(**params)
+    elif pred_algo == 'QDA':
+        clf = QuadraticDiscriminantAnalysis(**params)
+    elif pred_algo == 'GaussianP':
+        clf = GaussianProcessClassifier(**params)
+    elif pred_algo == 'NaiveB':
+        clf = GaussianNB(**params)   
+
+    clf = clf.fit(X_train, y_train)
+
+    # retrain the model
+    return clf
+
+def retrain_best_models2(result, X_train, y_train):
+    # Get info from best models and retrain them
+    
+    pred_algo = result[1] 
+
+    clf = train_model(pred_algo, X_train, y_train)
+
+    # retrain the model
+    return clf
+
+def get_data_from_table(table, features, outcome):
+
+    # get the data
+    X_train, X_val, y_train, y_test, features_list = get_dataset("/home/tachennf/Documents/delta-rad/extracted_radiomics/"+table, 
+        "/home/tachennf/Documents/delta-rad/extracted_radiomics/outcomes.csv", selection_method='fixed', outcome=outcome, sample_features=['Récidive Locale', 'Décès'])
+    
+    # normalize the data
+    X_train, X_val = min_max_scaling(X_train, X_val)
+
+    _, X_train_filtered, X_val_filtered = filter_dataset(X_train, X_val, features, features_list)
+
+    return X_train_filtered, X_val_filtered, y_train, y_test
+
+def filter_dataset(X_train: np.ndarray, X_val: np.ndarray, best_features: Sequence, feature_names: list): 
+    """
+    Filters the dataset to retain only the features selected by the algorithms.
+
+    Parameters:
+    X_train (np.ndarray): Training data array.
+    X_val (np.ndarray): Validation data array.
+    best_features (Sequence): Sequence of best features, can be a list or a dictionary.
+    nb_features (int): Number of top features to select.
+    feature_names (list): List of feature names corresponding to the columns in X_train and X_val.
+
+    Returns:
+    tuple: A tuple containing:
+        - selected_features (list): List of selected feature names.
+        - X_train_filtered (np.ndarray): Filtered training data array.
+        - X_val_filtered (np.ndarray): Filtered validation data array.
+    """
+
+    if type(best_features) == dict: 
+        best_features_dict = best_features
+        best_features = list(best_features_dict.keys())
+    elif type(best_features) == list: 
+        pass 
+    
+    X_train = pd.DataFrame(X_train, columns=feature_names)
+    X_val = pd.DataFrame(X_val, columns=feature_names)
+
+    # Use indices to filter the arrays
+    X_train_filtered = X_train[best_features]
+    X_val_filtered = X_val[best_features]
+
+    # transform back X_train_filtered to np.ndarray
+    X_train_filtered = X_train_filtered.to_numpy()
+    X_val_filtered = X_val_filtered.to_numpy()
+
+
+    return best_features, X_train_filtered, X_val_filtered
+
+def compare_roc_auc(top_results, outcome: str, nice_tables : list, cval: bool = True):
+    '''Plots the ROC AUC of the top models for each table and outcome.
+    Args:
+        top_results (dict): A dictionary containing the top results.
+        outcome (str): The outcome of interest.
+        nice_tables (list): A list of the nice names of the tables.
+        cval (bool): Whether to use cross-validation or not.
+    
+    Returns:
+        None
+    '''
+
+    plt.figure(figsize=(12, 4))
+    plt.plot([0, 1], [0, 1], color=(0.6, 0.6, 0.6), linestyle='--')
+    plt.plot([0, 0, 1], [0, 1, 1], lw=2, linestyle=':',
+             color='black', label='Perfect classifier')
+    k = 0 
+    for table in top_results.keys():
+        result = top_results[table][outcome][0]
+        features = result[3] 
+        X_train, X_test, y_train, y_test = get_data_from_table(table, features, outcome) # normalizes the data 
+
+        if cval: 
+            model = retrain_best_models2(result, X_train, y_train)
+        else:
+            model = retrain_best_models(result, X_train, y_train)
+        
+        y_test = y_test.astype('int64')
+        y_prob = model.predict_proba(X_test)[:, 1]
+        fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+        # sens, spe = compute_best_metrics(fpr, tpr, thresholds, y_prob, y_test)
+        # print(f"Table: {table}, Sensitivity: {sens}, Specificity: {spe}")
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw=2, label='%s | AUC = %0.3f' % (nice_tables[k], roc_auc))
+        k += 1
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('FPR (1-ESP)')
+    plt.ylabel('SEN')
+    plt.title(f'ROC-AUC Curve for {outcome}')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+def compute_best_metrics(fpr, tpr, thresholds, y_prob, y_true): 
+    '''Computes the best metrics for the best threshold according to Youden index.
+    Args:
+        fpr (array): The false positive rate.
+        tpr (array): The true positive rate.
+        thresholds (array): The thresholds.
+        y_prob (array): The predicted probabilities.
+        y_true (array): The true labels.
+    
+    Returns:
+        Sensitivity (float): The sensitivity.
+        Specificity (float): The specificity.
+    '''
+    youden_index = tpr - fpr
+    optimal_idx = np.argmax(youden_index)
+    optimal_threshold = thresholds[optimal_idx]
+    y_pred_optimal = (y_prob >= optimal_threshold).astype(int)
+    conf_matrix = confusion_matrix(y_true, y_pred_optimal)
+    tn, fp, fn, tp = conf_matrix.ravel()
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (tn + fp)
+
+    return sensitivity, specificity
 
 def find_perf_alg(results, delta_rad_tables, outcomes_list, feat_sel_algo_list, pred_algo_list, threshold: float = 0.7): 
     '''Find the algorithms with good performance based on a given threshold.
---
+++ rad_comparison/test.py
@@ -0,0 +1,23 @@
+import analysis_functions as af
+import json 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from collections import Counter
+import numpy as np
+
+results_dict = json.load(open('json_results/all_results_cv.json'))
+delta_rad_tables = ['f1_gtv.csv', 'f2_gtv.csv', 'f3_gtv.csv', 'f4_gtv.csv', 'f5_gtv.csv', 'rd_f1_f5_gtv.csv', 'rd_f1_f2_gtv.csv', 'rd_f1_f3_gtv.csv', 'rd_f2_f3_gtv.csv', 'rd_f1_f4_gtv.csv']
+feat_sel_algo_list = ['RF', 'ADABOOST', 'ANOVA_PERC', 'ANOVA_K_BEST', 'CHI2_PERC', 'CHI2_K_BEST', 'MI_PERC', 'MI_K_BEST', 'NO_SEL', 'RDM_SEL', 'LASSO']
+pred_algo_list = ['DT', 'RF', 'ADABOOST', 'PSVM', 'KNN', 'LOGREG', 'LOGREGRIDGE', 'BAGG', 'MLP', 'LDA', 'QDA', 'NaiveB'] 
+MAX_FEATURES = 3
+outcomes_list = ['Récidive Locale', 'Décès']
+metric_list = ['roc_auc', 'sensitivity', 'specificity']
+
+results = pd.read_json('json_results/all_results_cv.json') 
+
+
+top_results = af.get_best_results(results, delta_rad_tables, feat_sel_algo_list, pred_algo_list, 'Récidive Locale', metric='roc_auc', k = 1)
+
+nice_tables = ['F1','F2', 'F3', 'F4', 'F5', 'F1/F5', 'F1/F2', 'F1/F3', 'F2/F3', 'F1/F4']
+
+af.compare_roc_auc(top_results, 'Récidive Locale', nice_tables, cval=True)
