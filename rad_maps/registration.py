581376481a8ec355d0b0fbc5da30929ce9381c6b
Florent Tachenne <tachenne.florent@gmail.com>
2025-02-07 16:34:02 +0100

    added pipeline to register all images but still in progress

--- rad_maps/registration.py
+++ rad_maps/registration.py
@@ -61,7 +61,7 @@ def affine_registration(fixed_img_array: np.ndarray, moving_img_array: np.ndarra
     initial_transform = sitk.CenteredTransformInitializer(
         sitk.GetImageFromArray(fixed_img_array),
         sitk.GetImageFromArray(moving_img_array),
-        sitk.AffineTransform(3),
+        sitk.Euler3DTransform(),
         sitk.CenteredTransformInitializerFilter.GEOMETRY,
     )
 
@@ -83,16 +83,16 @@ def affine_registration(fixed_img_array: np.ndarray, moving_img_array: np.ndarra
         metric_values.append(registration_method.GetMetricValue())
 
     # Similarity metric settings.
-    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=100)
     registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
-    registration_method.SetMetricSamplingPercentage(0.01)
+    registration_method.SetMetricSamplingPercentage(0.5)
 
     # Interpolator settings.
     registration_method.SetInterpolator(sitk.sitkLinear)
 
     # Optimizer settings.
-    registration_method.SetOptimizerAsGradientDescent(
-        learningRate=0.01,
+    registration_method.SetOptimizerAsGradientDescentLineSearch( #SetOptimizerAsGradientDescent 
+        learningRate=1,
         numberOfIterations=100
     )
     registration_method.SetOptimizerScalesFromPhysicalShift()
@@ -103,13 +103,14 @@ def affine_registration(fixed_img_array: np.ndarray, moving_img_array: np.ndarra
     registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
 # 
     # # Don't optimize in-place, we would possibly like to run this cell multiple times.
-    registration_method.SetInitialTransform(initial_transform, inPlace=False)
+    final_transform = sitk.Euler3DTransform(initial_transform)
+    registration_method.SetInitialTransform(final_transform) # , inPlace=False
 
     registration_method.AddCommand(sitk.sitkIterationEvent, update_metric)
 
     final_transform = registration_method.Execute(sitk.GetImageFromArray(fixed_img_array), moving_resampled)
     
-    final_transform = final_transform.GetBackTransform()
+    # final_transform = final_transform.GetBackTransform()
 
     return registration_method, final_transform, metric_values
 
@@ -136,12 +137,28 @@ def apply_3D_transform(moving_3D_array: np.ndarray, transform: sitk.Transform, m
     return sitk.GetArrayFromImage(resampled_image)
 
 def apply_3D_transform2(moving_3D_array: np.ndarray, fixed_3D_array: np.ndarray, transform: sitk.Transform, mask: bool = False) -> np.ndarray:
+    ''' Apply a 3D transformation to an image.
+    
+    Parameters:
+        moving_3D_array (np.ndarray): The image to transform.
+        fixed_3D_array (np.ndarray): The reference image.
+        transform (sitk.Transform): The transformation to apply.
+        mask (bool): Whether the image is a mask. Changes the interpolation method.
+        
+    Returns:
+        np.ndarray: The transformed image.
+    '''
+
     moving_image = sitk.GetImageFromArray(moving_3D_array)
     fixed_image = sitk.GetImageFromArray(fixed_3D_array)
     if mask: 
         interp = sitk.sitkNearestNeighbor
     else:
         interp = sitk.sitkLinear
+
+    # print(transform.GetTranslation())
+    # transform.Translate((0, 0, 8))
+    # print(transform.GetTranslation())
 
     moving_resampled = sitk.Resample(
     moving_image,
@@ -154,8 +171,128 @@ def apply_3D_transform2(moving_3D_array: np.ndarray, fixed_3D_array: np.ndarray,
 
     return sitk.GetArrayFromImage(moving_resampled)
 
+def match_histograms(image: np.ndarray, reference: np.ndarray):
+    ''' Match the histogram of an image to a reference image.
+    
+    Parameters:
+        image (np.ndarray): The image to match.
+        reference (np.ndarray): The reference image.
+        
+    Returns:
+        np.ndarray: The matched image.
+    '''
+    
+    image = sitk.GetImageFromArray(image)
+    reference = sitk.GetImageFromArray(reference)
+    
+    matched = sitk.HistogramMatching(image, reference)
+    
+    return sitk.GetArrayFromImage(matched)
+
+def resize_image(image, new_size, interpolator=sitk.sitkLinear):
+    """
+    Resizes a SimpleITK image to the specified new_size.
+
+    Parameters:
+        image (sitk.Image): Input SimpleITK image.
+        new_size (tuple): Desired output size (x, y, z).
+        interpolator: Interpolation method (default: sitk.sitkLinear).
+
+    Returns:
+        sitk.Image: Resized image.
+    """
+    original_size = image.GetSize()  # Get original image size
+    original_spacing = image.GetSpacing()  # Get original spacing
+
+    # Compute new spacing to preserve aspect ratio
+    new_spacing = [
+        (original_size[i] * original_spacing[i]) / new_size[i]
+        for i in range(3)
+    ]
+
+    # Resample the image
+    resampled_image = sitk.Resample(
+        image,
+        new_size,
+        sitk.Transform(),  # Identity transform
+        interpolator,  # Linear interpolation (default)
+        image.GetOrigin(),
+        new_spacing,
+        image.GetDirection(),
+        0,  # Default pixel value (background)
+        image.GetPixelID()
+    )
+
+    return resampled_image
+
+def register_gtv(simu_path: str, f_path: str, gtv_simu_path: str, gtv_f_path: str, output_path: str, normalization: str = 'zscore'):
+    ''' Register the GTVs of a fraction image to the simulation image.
+    
+    Parameters:
+        simu_path (str): The path to the simulation image.  
+        f_path (str): The path to the fraction image.
+        gtv_simu_path (str): The path to the GTV of the simulation image.
+        gtv_f_path (str): The path to the GTV of the fraction image.
+        output_path (str): The path to save the registered GTV.
+        normalization (str): The normalization method to use. Options are 'zscore', 'histogram' and None.
+
+    Returns:
+        float: The MSE before registration.
+        float: The MSE after registration.
+    '''
+    
+    # charger simu 
+    simu = sitk.ReadImage(simu_path)
+    simu = sitk.GetArrayFromImage(simu)
+
+    # charger image F5
+    fraction = sitk.ReadImage(f_path)
+    fraction = sitk.GetArrayFromImage(fraction)
+
+    if simu.shape != fraction.shape:
+        print("Images have different shapes.")
+        return None, None 
+
+    # normalize images
+    if normalization == 'zscore':
+        simu = (simu - np.mean(simu)) / np.std(simu)
+        fraction = (fraction - np.mean(fraction)) / np.std(fraction)
+    elif normalization == 'histogram':
+        fraction = match_histograms(fraction, simu)
+    else:
+        pass
+
+    registration_method, T, metric_values = affine_registration(simu, fraction)
+
+    # COMPARE GTVs 
+    gtv_simu = sitk.ReadImage(gtv_simu_path) # charger gtv simu 
+    gtv_simu = sitk.GetArrayFromImage(gtv_simu)
+    gtv_fraction = sitk.ReadImage(gtv_f_path) # charger gtv F5 
+    gtv_fraction = sitk.GetArrayFromImage(gtv_fraction)
+
+    mse_before = compute_mse(gtv_simu, gtv_fraction)
+    # print("MSE before registration: ", mse_before)
+
+    registered_gtv_fraction = apply_3D_transform2(gtv_fraction, gtv_simu, T, mask=True)
+
+    # comparer gtv simu et F5 
+    mse_after = compute_mse(gtv_simu, registered_gtv_fraction)
+    # print("MSE after registration:", mse_after)
+
+    # sauvegarder les images
+    transformed_img = sitk.GetImageFromArray(registered_gtv_fraction)
+    transformed_img = sitk.TransformGeometry(transformed_img, T)
+# 
+    sitk.WriteImage(transformed_img, output_path)
+
+    return mse_before, mse_after
+
 def compute_mse(y_true, y_pred): 
     return np.mean((y_true - y_pred)**2)
+
+def compute_dice(y_true, y_pred):
+    intersection = np.sum(y_true * y_pred)
+    return 2 * intersection / (np.sum(y_true) + np.sum(y_pred))
 
 
 # Callback invoked when the StartEvent happens, sets up our new data.
--- rad_maps/test_registration.py
+++ rad_maps/test_registration.py
@@ -2,8 +2,10 @@ import registration as r
 import nibabel as nib  
 import SimpleITK as sitk 
 import matplotlib.pyplot as plt 
+import numpy as np
+import os 
 
-def main(): 
+def test_registration(): 
     # charger simu 
     simu = sitk.ReadImage('/home/tachennf/Documents/delta-rad/rad_maps/Data/Patient76/img_dir/Patient76_IRM_simu_mridian.nii')
     simu = sitk.GetArrayFromImage(simu)
@@ -24,50 +26,100 @@ def main():
     plt.ylabel("Metric Value (Mutual Information)")
     plt.title("Convergence of Registration")
     plt.show()
-
     print(f"Final metric value: {registration_method.GetMetricValue()}")
     print(
         f"Optimizer's stopping condition, {registration_method.GetOptimizerStopConditionDescription()}"
     )
     
-    registered_f5 = r.apply_3D_transform2(f5, simu, T, mask=False)
+    # registered_f5 = r.apply_3D_transform2(f5, simu, T, mask=False)
 
     # COMPARE GTVs 
-
     gtv_simu = sitk.ReadImage('/home/tachennf/Documents/delta-rad/rad_maps/Data/Patient76/mask_dir/Patient76_IRM_simu_mridian_gtv.nii') # charger gtv simu 
     gtv_simu = sitk.GetArrayFromImage(gtv_simu)
     gtv_f5 = sitk.ReadImage('/home/tachennf/Documents/delta-rad/rad_maps/Data/Patient76/mask_dir/Patient76_mridian_ttt_5_gtv.nii') # charger gtv F5 
     gtv_f5 = sitk.GetArrayFromImage(gtv_f5)
 
-    mse_before = r.compute_mse(gtv_simu, gtv_f5)
-    print("MSE before registration: ", mse_before)
+    mse_before = r.compute_dice(gtv_simu, gtv_f5)
+    print("Dice before registration: ", mse_before)
 
     registered_gtv_f5 = r.apply_3D_transform2(gtv_f5, gtv_simu, T, mask=True)
-    # comparer gtv simu et F5 
 
-    mse_after = r.compute_mse(gtv_simu, registered_gtv_f5)
-    print("MSE after registration:", mse_after)
+    # comparer gtv simu et F5 
+    mse_after = r.compute_dice(gtv_simu, registered_gtv_f5)
+    print("Dice after registration:", mse_after)
 
     # plt.figure()
     # plt.subplot(131), plt.imshow(gtv_simu[:, :, 36]), plt.title('Simu')
     # plt.subplot(132), plt.imshow(gtv_f5[:, :, 36]), plt.title('F5')
     # plt.subplot(133), plt.imshow(registered_gtv_f5[:, :, 36]), plt.title('R-F5')
     # plt.show()
-    print(registered_gtv_f5.shape)
+
     # sauvegarder les images
     transformed_img = sitk.GetImageFromArray(registered_gtv_f5)
     transformed_img = sitk.TransformGeometry(transformed_img, T)
 
-    f5_img = sitk.GetImageFromArray(f5)
-
-    print(
-        f"origin before: {f5_img.GetOrigin()}\norigin after: {transformed_img.GetOrigin()}"
-    )
-    print(
-        f"direction cosine before: {f5_img.GetDirection()}\ndirection cosine after: {transformed_img.GetDirection()}"
-    )
+    # Print information about image 
+    # f5_img = sitk.GetImageFromArray(f5)
+    # print(f"origin before: {f5_img.GetOrigin()}\norigin after: {transformed_img.GetOrigin()}")
+    # print(f"direction cosine before: {f5_img.GetDirection()}\ndirection cosine after: {transformed_img.GetDirection()}")
 
     sitk.WriteImage(transformed_img, '/home/tachennf/Documents/delta-rad/rad_maps/Data/Patient76/mask_dir/registered_Patient76_mridian_ttt_5_gtv.nii')
 
+def main():
+    patient_list = [p for p in os.listdir('/home/tachennf/Documents/delta-rad/rad_maps/Data/')]
+    patient_list = ['Patient76']
+    for p in patient_list:
+        forbidden_patients = ['Patient32', 'Patient56', 'Patient57', 'Patient66', 'Patient14', 'Patient27', 'Patient80']
+        if p in forbidden_patients:
+            continue
+        print(f"{p}")
+
+        # check that we have patient folder 
+        if os.path.exists(f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}') == False: 
+            continue
+        simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/img_dir/{p}_IRM_simu_mridian.nii'
+        if os.path.exists(simu_path) or os.path.exists(f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/img_dir/{p}_IRM_MRIdian.nii'): # simu exists
+            for i in range(1, 6): 
+                
+                f_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/img_dir/{p}_mridian_ttt_{i}.nii'
+                if os.path.exists(f_path) == False: # if fraction is missing 
+                    continue
+
+                gtv_simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/{p}_IRM_simu_mridian_gtv.nii'
+                gtv_f_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/{p}_mridian_ttt_{i}_gtv.nii'
+
+                output_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/registered_{p}_mridian_ttt_{i}_gtv.nii'
+
+                if os.path.exists(simu_path): # standard path 
+                    mse_before, mse_after = r.register_gtv(simu_path, f_path, gtv_simu_path, gtv_f_path, output_path, normalization='histogram')
+
+                else: # MRIdian path 
+                    simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/img_dir/{p}_IRM_simu_MRIdian.nii'
+                    gtv_simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/{p}_IRM_simu_MRIdian_gtv.nii'
+                    mse_before, mse_after = r.register_gtv(simu_path, f_path, gtv_simu_path, gtv_f_path, output_path, normalization='histogram')
+
+        else: # simu does not exist 
+            for i in range(2, 6): 
+                simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/img_dir/{p}_mridian_ttt_1.nii' # simu is F1 
+                
+                f_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/img_dir/{p}_mridian_ttt_{i}.nii'
+                if os.path.exists(f_path) == False: # if fraction is missing 
+                    continue
+
+                gtv_simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/{p}_mridian_ttt_1_gtv.nii'
+                gtv_f_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/{p}_mridian_ttt_{i}_gtv.nii'
+
+                output_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/registered_{p}_mridian_ttt_{i}_gtv.nii'
+
+                if os.path.exists(simu_path): # standard path 
+                    mse_before, mse_after = r.register_gtv(simu_path, f_path, gtv_simu_path, gtv_f_path, output_path, normalization='histogram')
+
+                else: # MRIdian path 
+                    simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/img_dir/{p}_IRM_MRIdian.nii'
+                    gtv_simu_path = f'/home/tachennf/Documents/delta-rad/rad_maps/Data/{p}/mask_dir/{p}_IRM_MRIdian_gtv.nii'
+                    mse_before, mse_after = r.register_gtv(simu_path, f_path, gtv_simu_path, gtv_f_path, output_path, normalization='histogram')
+    
+
 if __name__ == '__main__': 
-    main()
+    #main()
+    test_registration()
